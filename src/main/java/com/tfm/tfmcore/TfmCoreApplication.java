f5ca9f40090656259b6e0f615cc2edc5c7635fac
danielhdzf <danielsan13a@gmail.com>
2024-04-29 16:18:51 +0100

    Initialize project danielhdzf/tfm#2

---
+++ src/main/java/com/tfm/tfmcore/TfmCoreApplication.java
@@ -0,0 +1,13 @@
+package com.tfm.tfmcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TfmCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TfmCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tfm/tfmcore/TfmCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tfm.tfmcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TfmCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
