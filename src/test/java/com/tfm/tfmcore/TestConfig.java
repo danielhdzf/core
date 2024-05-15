c099c91af5263a433c610037766a192da36feefb
danielhdzf <danielsan13a@gmail.com>
2024-05-15 14:41:07 +0100

    Fix test action

--- src/test/java/com/tfm/tfmcore/TestConfig.java
+++ src/test/java/com/tfm/tfmcore/TestConfig.java
@@ -7,12 +7,14 @@ import java.lang.annotation.RetentionPolicy;
 
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 
 @ExtendWith(SpringExtension.class)
+@TestPropertySource(locations = "classpath:test.properties")
 @SpringBootTest
 public @interface TestConfig {
 }
